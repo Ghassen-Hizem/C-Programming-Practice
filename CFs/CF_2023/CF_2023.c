47d5660684f70191d8bcb81a3382ceafb109fe1f
hizem <ghassen.hizem@supcom.tn>
2024-04-17 20:12:45 +0200

    Cf 2023

---
+++ CFs/CF_2023/CF_2023.c
@@ -0,0 +1,12 @@
+//
+// Created by hizem on 4/17/24.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+
+
+    return 0;
+}
--- TP4/pointeurs.c
+++ TP4/pointeurs.c
@@ -1,9 +1,31 @@
+//Library : General utilities
 #include <stdlib.h>
-#include <stdint.h>
+//Library : fonctions usuelles de C (input + output)
 #include <stdio.h>
 
+void squares(int* a , int* b) {
+    *a = *a * *a;
+    *b = *b * *b;
+}
 
 int main() {
-    system("ls");
+    int a =4;
+    int b = 0;
+    int* pa = &a;
+    int* pb = &b;
+
+   // printf("valeurs sont: %d , %d",a,b);
+
+   printf("les valeurs des pointeurs sont : %d , %d\n",*pa,*pb);
+   printf("Les adresses des pointeurs sont : %p, %p\n",pa,pb);
+
+
+       b = *pb + 9;
+    printf("valeurs sont: %d , %d\n",a,b);
+
+    squares(&a,&b);
+    printf("valeurs sont: %d , %d\n",a,b);
     return 0;
 }
+
+
